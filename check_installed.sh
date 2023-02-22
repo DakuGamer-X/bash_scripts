c67b211750439927f92893b7aa80af8314333bd5
Carlos X <122121098+DakuGamer-X@users.noreply.github.com>
2023-02-22 13:56:11 -0500

    Delete check_installed.sh

--- check_installed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter the name of the software to search for: " software_name
-
-if yum list installed "$software_name" >/dev/null 2>&1; then
-    echo "$software_name is installed"
-else
-    echo "$software_name is not installed"
-fi
